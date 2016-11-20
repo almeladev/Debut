3cf7c8543b87d272799941497430bc9c7da0969b
Daniel Martínez <daniel@holamundo.me>
2016-11-20 15:39:21 +0100

    Vagrant LAMP actualizado con Ubuntu 16.04

--- _install/config.sh
+++ _install/config.sh
@@ -3,17 +3,18 @@
 # config.sh - Configuración para Vagrant LAMP con Debut
 #
 # Archivo de configuración automática para la box 
-# Ubuntu 14.04 en Vagrant.
+# Ubuntu 16.04 en Vagrant.
 #
 # Autor: Daniel Martínez <danmnez.me>
 # Creado: 14.09.2016
+# Actualizado: 20.11.2016
 # ======================================================= #
 
 # ======================================================= #
 # Variables y funciones globales
 # ======================================================= #
 PROJECTFOLDER='www' # Nombre del directorio para los proyectos.
-PASSWORD='123' # Contraseña de MySQL.
+MYSQL_PASSWORD='123' # Contraseña de MySQL.
 DATABASE_SQL='ejemplo.sql' # Base de datos.
 GIT_REPOS='https://github.com/DanMnez/Debut.git' # Repositorios del proyecto.
 
@@ -37,25 +38,19 @@ sudo apt-get install apache2 -y
 # Instalación de php 7 
 # ======================================================= #
 sudo apt-get install python-software-properties -y
-sudo add-apt-repository ppa:ondrej/php
 update
-
-sudo apt-get install php7.0-cli php7.0 libapache2-mod-php7.0 php7.0-mysql -y
-sudo apt-get install php7.0-cgi php7.0-dbg php7.0-dev php7.0-curl php7.0-gd -y
-sudo apt-get install php7.0-mcrypt php7.0-xsl php7.0-intl -y
+sudo apt-get install -y php libapache2-mod-php php-mcrypt php-mysql \
+                        php-curl php-cli
 
 # ======================================================= #
 # Instalación de mysql 5.7 
 # ======================================================= #
 export DEBIAN_FRONTEND=noninteractive # Forzamos al sistema a no tener interacción manual.
-apt-key adv --keyserver ha.pool.sks-keyservers.net --recv-keys 5072E1F5 
-echo "deb http://repo.mysql.com/apt/ubuntu/ trusty mysql-5.7" | tee -a /etc/apt/sources.list.d/mysql.list
 update
 
-debconf-set-selections <<< "mysql-community-server mysql-community-server/data-dir select ''"
-debconf-set-selections <<< "mysql-community-server mysql-community-server/root-pass password $PASSWORD"
-debconf-set-selections <<< "mysql-community-server mysql-community-server/re-root-pass password $PASSWORD"
-sudo apt-get install mysql-server -y
+sudo debconf-set-selections <<< "mysql-server-5.7 mysql-server/root_password password $MYSQL_PASSWORD"
+sudo debconf-set-selections <<< "mysql-server-5.7 mysql-server/root_password_again password $MYSQL_PASSWORD"
+sudo apt-get install mysql-server -y # Instalar MySQL
 
 # ======================================================= #
 # Creación del directorio del proyecto
@@ -114,7 +109,7 @@ composer install
 # ======================================================= #
 # Carga la base de datos
 # ======================================================= #
-sudo mysql -h "localhost" -u "root" "-p${PASSWORD}" < "/var/www/html/${PROJECTFOLDER}/_database/${DATABASE_SQL}"
+sudo mysql -h "localhost" -u "root" "-p${MYSQL_PASSWORD}" < "/var/www/html/${PROJECTFOLDER}/_database/${DATABASE_SQL}"
 
 # ======================================================= #
 # Mensaje de fin
